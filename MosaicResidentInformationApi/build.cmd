6628585bf24ce418994f828f61567541c947de83
LBHTKarki <58432863+LBHTKarki@users.noreply.github.com>
2021-02-24 11:58:36 +0000

    increase memory size and update build settings

--- MosaicResidentInformationApi/build.cmd
+++ MosaicResidentInformationApi/build.cmd
@@ -1,2 +1,2 @@
 dotnet restore
-dotnet lambda package --configuration release --framework netcoreapp3.1 --output-package bin/release/netcoreapp3.1/mosaic-resident-information-api.zip
+dotnet lambda package --configuration release --framework netcoreapp3.1 --msbuild-parameters "/p:PublishReadyToRun=true /p:TieredCompilation=false /p:TieredCompilationQuickJit=false" --output-package bin/release/netcoreapp3.1/mosaic-resident-information-api.zip
