d1617089b33233ff125407af2f2eb57d581ade34
Renny Fadoju <rennyvbirds@yahoo.com>
2021-03-15 11:51:36 +0000

    Configure test database in docker to be accessible via localhost:6543
    We found out that if postgres was running locally on our machines, whenever
    we ran the tests via our IDE (e.g Rider), since the port configured for our docker
    test database was the same as default postgres port, this caused a local instance
    of the test database to be created rather than a connection being made to the one in
    the docker container.

--- ResidentsSocialCarePlatformApi.Tests/ConnectionString.cs
+++ ResidentsSocialCarePlatformApi.Tests/ConnectionString.cs
@@ -7,7 +7,7 @@ namespace ResidentsSocialCarePlatformApi.Tests
         public static string TestDatabase()
         {
             return $"Host={Environment.GetEnvironmentVariable("DB_HOST") ?? "127.0.0.1"};" +
-                   $"Port={Environment.GetEnvironmentVariable("DB_PORT") ?? "5432"};" +
+                   $"Port={Environment.GetEnvironmentVariable("DB_PORT") ?? "6543"};" +
                    $"Username={Environment.GetEnvironmentVariable("DB_USERNAME") ?? "postgres"};" +
                    $"Password={Environment.GetEnvironmentVariable("DB_PASSWORD") ?? "mypassword"};" +
                    $"Database={Environment.GetEnvironmentVariable("DB_DATABASE") ?? "testsocialcare"}";
