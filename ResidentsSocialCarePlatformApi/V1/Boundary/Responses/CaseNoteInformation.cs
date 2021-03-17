ee5493e01bf24c11501739b84e18cc9e053e12eb
Wen Ting Wang <wenting@madetech.com>
2021-03-17 16:51:20 +0000

    Add ToResponse method for Domain.CaseNoteInformation

--- ResidentsSocialCarePlatformApi/V1/Boundary/Responses/CaseNoteInformation.cs
+++ ResidentsSocialCarePlatformApi/V1/Boundary/Responses/CaseNoteInformation.cs
@@ -8,7 +8,7 @@ namespace ResidentsSocialCarePlatformApi.V1.Boundary.Responses
 
         public long CaseNoteId { get; set; }
 
-        public int PersonVisitId { get; set; }
+        public long PersonVisitId { get; set; }
 
         public string NoteType { get; set; }
 
@@ -30,15 +30,17 @@ namespace ResidentsSocialCarePlatformApi.V1.Boundary.Responses
 
         public string CaseNoteContent { get; set; }
 
-        public int RootCaseNoteId { get; set; }
+        public long RootCaseNoteId { get; set; }
 
         public string CompletedDate { get; set; }
 
         public string TimeoutDate { get; set; }
 
-        public int CopyOfCaseNoteId { get; set; }
+        public long CopyOfCaseNoteId { get; set; }
 
-        public string CopiedBy { get; set; }
+        public string CopiedByName { get; set; }
+
+        public string CopiedByEmail { get; set; }
 
         public string CopiedDate { get; set; }
     }
