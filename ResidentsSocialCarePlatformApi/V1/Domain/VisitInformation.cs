ab8259198b39db6913a1ddcc2016dde492d80a06
JohnFarrellDev <johnfarrell@fastmail.com>
2021-03-24 09:56:23 +0000

    create Domain class for VisitInformation

---
+++ ResidentsSocialCarePlatformApi/V1/Domain/VisitInformation.cs
@@ -0,0 +1,38 @@
+using System;
+#nullable enable
+
+namespace ResidentsSocialCarePlatformApi.V1.Domain
+{
+    public class VisitInformation
+    {
+        public long VisitId { get; set; }
+
+        public long PersonId { get; set; }
+
+        public string VisitType { get; set; } = null!;
+
+        public DateTime? PlannedDateTime { get; set;  }
+
+        public DateTime? ActualDateTime { get; set;  }
+
+        public string? ReasonNotPlanned { get; set; }
+
+        public string? ReasonVisitNotMade { get; set; }
+
+        public string? SeenAloneFlag { get; set; }
+
+        public string? CompletedFlag { get; set; }
+
+        public long? OrgId { get; set; }
+
+        public long? WorkerId { get; set; }
+
+        public long? CpRegistrationId { get; set; }
+
+        public long? CpVisitScheduleStepId { get; set; }
+
+        public long? CpVisitScheduleDays { get; set; }
+
+        public string? CpVisitOnTime { get; set; }
+    }
+}
